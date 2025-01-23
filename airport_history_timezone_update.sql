ea07ae64f6e423afd2ceb0ca7dba9b9dc1512684
Sylwester <sylwesterf@users.noreply.github.com>
2025-01-23 23:37:18 +0100

    Update and rename airport_history_timezone_update_mk1.sql to airport_history_timezone_update.sql

--- airport_history_timezone_update.sql
+++ airport_history_timezone_update.sql
@@ -4,8 +4,6 @@ The dimension table from OAI does not contain this data, so we have to load the 
 , and then update the airport history dimension: 
 */
 
-create schema geography;
-
 -- Shape file for time zone boundaries was located here:
 -- https://github.com/evansiroky/timezone-boundary-builder/releases/download/2023b/timezones-with-oceans.shapefile.zip
 -- This is after the shape file was loaded via shp2pgsql command line tool:
@@ -13,8 +11,6 @@ create schema geography;
 
 select * from public."combined-shapefile-with-oceans" limit 10;
 alter table public."combined-shapefile-with-oceans" rename to timezone_boundaries;
-alter table public.timezone_boundaries set schema geography;
-alter table geography.timezone_boundaries rename to time_zone_boundaries;
 
 SELECT * FROM air_oai_dims.airport_history limit 100;
 SELECT time_zone_name, count(*) FROM air_oai_dims.airport_history group by 1 order by count(*) desc;
@@ -25,7 +21,7 @@ from (
 select a.airport_history_id, a.airport_oai_code, a.effective_from_date, b.time_zone_name 
 from (select airport_history_id, airport_oai_code, effective_from_date, point_geom 
       from air_oai_dims.airport_history where time_zone_name is null /*limit 10000*/) a
-cross join (select gid, tzid as time_zone_name, geom as time_zone_geom from geography.time_zone_boundaries) b
+cross join (select gid, tzid as time_zone_name, geom as time_zone_geom from public.timezone_boundaries) b
 where ST_Contains(b.time_zone_geom, a.point_geom) is true
 ) c
 where air_oai_dims.airport_history.airport_history_id = c.airport_history_id
